961152e7017136a95e8e5d295da477676d57bb2b
Simon Thurman <simonthurman@hotmail.com>
2025-02-27 08:39:14 +0000

    Update ValuesController.cs

--- MyProductsAPI/Controllers/ValuesController.cs
+++ MyProductsAPI/Controllers/ValuesController.cs
@@ -13,11 +13,11 @@ namespace MyProductsAPI.Controllers
     {
         Product[] products = new Product[]
 {
-            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 1 },
-            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 3.75M},
-            new Product { Id = 3, Name = "Drum", Category = "Toys", Price = 3.75M },
-            new Product { Id = 4, Name = "Chair", Category = "Furniture", Price = 3.75M },
-            new Product { Id = 5, Name = "XboX", Category = "Toys", Price = 3.75M },
+            new Product { Id = 1, Name = "Tomato Soup", Category = "Groceries", Price = 3 },
+            new Product { Id = 2, Name = "Yo-yo", Category = "Toys", Price = 9.75M},
+            new Product { Id = 3, Name = "Drum", Category = "Toys", Price = 15.55M },
+            new Product { Id = 4, Name = "Chair", Category = "Furniture", Price = 30.25M },
+            new Product { Id = 5, Name = "XboX", Category = "Toys", Price = 133.55M },
             new Product { Id = 6, Name = "Hammer", Category = "Hardware", Price = 16.99M }
 };
 
